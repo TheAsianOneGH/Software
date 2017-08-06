701884d341cf1ed6597400c8fb996cc6f1db4114
TheAsianOneGH <evermakegeneration@gmail.com>
2017-08-06 04:01:44 -0600

    Update style.js

--- JavaScript/GitHub/Style/Version-1.0/style.js
+++ JavaScript/GitHub/Style/Version-1.0/style.js
@@ -11,6 +11,7 @@ function addDropDown(id,text){
  document.getElementsByTagName("BODY")[0].innerHTML += content;
  var style = '.dropbtn{background-color:#FFFFFF;color:#00FFFF;padding:16px;font-size:16px;border:none;cursor:pointer; }';
  style += '.dropContent{display:none;}';
+ style += '.dropContent a{padding:12px 16px;}';
  style += '.dropdown{position:relative;display:inline-block;}';
  style += '.dropdown:hover .dropContent{display:block;}';
  document.getElementsByTagName("STYLE")[0].innerHTML += style;
