10d8425fd03393cab7cd8dae503878508fcf97dc
TheAsianOneGH <evermakegeneration@gmail.com>
2017-08-06 04:12:29 -0600

    Update style.js

--- JavaScript/GitHub/Style/Version-1.0/style.js
+++ JavaScript/GitHub/Style/Version-1.0/style.js
@@ -7,16 +7,16 @@ function addStyleTag(style){
  document.getElementsByTagName("HEAD")[0].innerHTML += '<style>body{font-family:'+style+'}</style>';
 }
 function addDropDown(id,text){
- var content = '<div class="dropdown" id=';content += id;content += '><p class="dropbtn">';content += text;content += '</p><div class="dropContent" id="c';content += id;content += '"></div></div>';
+ var content = '<div class="dropdown" id=';content += id;content += '><h2 class="dropbtn">';content += text;content += '</h2><div class="dropContent" id="c';content += id;content += '"></div></div>';
  document.getElementsByTagName("BODY")[0].innerHTML += content;
- var style = '.dropbtn{background-color:#FFFFFF;color:#00FFFF;padding:16px;font-size:72;border:none;cursor:pointer; }';
+ var style = '.dropbtn{background-color:#FFFFFF;color:#00FFFF;padding:16px;border:none;cursor:pointer; }';
  style += '.dropContent{background-color:#EEEEEE;display:none;}';
- style += '.dropContent a{display:block;color:#FFFFFF;padding:12px 16px;text-decoration:none;}';
+ style += '.dropContent h3{display:block;color:#FFFFFF;padding:12px 16px;text-decoration:none;}';
  style += '.dropdown{position:relative;display:inline-block;}';
  style += '.dropdown:hover .dropContent{display:block;}';
  document.getElementsByTagName("STYLE")[0].innerHTML += style;
 }
 function addDropItem(id,text){
- var content = '<a href="#">';content += text;content += '</a>';
+ var content = '<h3>';content += text;content += '</h3>';
  document.getElementById("c"+id).innerHTML += content;
 }
