26350d79d5e73bb22364a7490f194a4b0cd69910
TheAsianOneGH <evermakegeneration@gmail.com>
2017-08-06 03:45:41 -0600

    Update style.js

--- JavaScript/GitHub/Style/Version-1.0/style.js
+++ JavaScript/GitHub/Style/Version-1.0/style.js
@@ -3,7 +3,7 @@ var STYLE = {
  addDrop: function(id,text){ addDropDown(id,text); }
 };
 function addStyleTag(style){
- document.getElementsByTagName("HEAD")[0].innerHTML += '<style>body{font-family:'+style+'</style>';
+ document.getElementsByTagName("HEAD")[0].innerHTML += '<style>body{font-family:'+style+'}</style>';
 }
 function addDropDown(id,text){
  var content = '<div class="dropdown" id=';content += id;content += '><p class="dropbtn">';content += text;content += '</p><div class="dropContent"></div></div>';
